4cd2e5b14d0adf402430158898bccb90150ebd31
Bleak Chandler <bleakchandler@gmail.com>
2021-03-22 21:17:09 -0400

    Added some changes

--- src/index.js
+++ src/index.js
@@ -1,3 +1,8 @@
+const getToyRequest = 'http://localhost:3000/toys'
+const toyCollection = document.querySelector("#toy-collection")
+const form = document.querySelector(".add-toy-form")
+
+
 let addToy = false;
 
 document.addEventListener("DOMContentLoaded", () => {
@@ -13,3 +18,74 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 });
+
+function getToy() {
+  fetch(getToyRequest)
+  .then(response => response.json())
+  .then(toysObject => {
+    toysObject.forEach(toysObject => renderToys(toysObject))
+  })
+}
+
+function renderToys(toy) {
+  const div = document.createElement('div') // With the response data, make a <div class="card"> for each toy and add it to the toy-collection div.
+  div.dataset.id = toy.id
+  let {name, image, likes} = toy
+  div.innerHTML = `
+  <h2>${name}</h2>
+  <img src=${image} class="toy-avatar" />
+  <p>${likes} Likes </p>
+  <button class="like-btn">Like <3</button>`
+  toyCollection.append(div)
+}
+
+getToy ()
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault()
+  const name = event.target.name.value
+  const image = event.target.image.value
+
+  fetch(getToyRequest, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      Accept: "application/json"
+    },
+    
+    body: JSON.stringify({
+      name,
+      image,
+      likes: 0
+    })
+  })
+  .then(response => response.json)
+  .then(newToy => {
+    renderToys(newToy)
+    event.target.reset()
+  })
+})
+
+toyCollection.addEventListener('click', (event) => {
+  if (event.target.matches("button.like-btn")) {
+    const div = event.target.closest('div')
+    const pLikes = event.target.previousElementSibling
+    const newLikes = parseInt(pLikes.textContent) + 1
+    fetch(`${getToyRequest}/${div.dataset.id}`, {
+      method: "PATCH",
+      headers: {
+        "content-type": "application/json",
+        accept: "application/json"
+      },
+      body: JSON.stringify({
+        likes: newLikes
+      })
+    })
+    .then(response => response.json())
+    .then(data => pLikes.textContent = `${data.likes} Likes`)
+  }
+})
+
+
+
+
